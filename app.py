180398a1c7f7f141d193462f931e37eef70ee996
enesertas <enesertas@gmail.com>
2024-04-09 01:29:26 +0300

    minor tweaks

--- app.py
+++ app.py
@@ -7,10 +7,18 @@ import cv2
 import time
 import sys
 import os
+import torch
+
+from syncdiffusion.syncdiffusion_model import SyncDiffusion
+from syncdiffusion.utils import seed_everything
 
 app = Flask(__name__)
 CORS(app)
 url_public = sys.argv[1]
+device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+
+# Load SyncDiffusion model
+syncdiffusion_model = SyncDiffusion(device, sd_version="2.0")
 
 @app.route("/", methods=['GET'])
 def home():
@@ -18,23 +26,27 @@ def home():
 
 @app.route("/generate-image", methods=['GET'])
 def generate():
+
+    seed_everything(100)
+
+    # Generate images
+    img = syncdiffusion_model.sample_syncdiffusion(
+        prompts = "a photo of a mountain range at twilight",
+        negative_prompts = "",
+        height = 512,
+        width = 3072,
+        num_inference_steps = 20,
+        guidance_scale = 7.5,
+        sync_weight = 20.0,
+        sync_decay_rate = 0.95,
+        sync_freq = 1,
+        sync_thres = 10,
+        stride = 16,
+    )
+    img.save("./flask-app-design-project/templates/result.jpg")
+    print(f"[INFO] saved the result")
     
-    # generate syncdiffusion image, encode in base64 and send in json
-    os.system("""python3 ./flask-app-design-project/SyncDiffusion/sample_syncdiffusion.py \
---prompt "a photo of a mountain range at twilight" \
---negative "" \
---H 512 \
---W 3072 \
---seed 100 \
---steps 50 \
---sync_weight 20.0 \
---sync_decay_rate 0.95 \
---sync_freq 1 \
---sync_thres 10 \
---sd_version "2.0" \
---save_dir "results" \
---stride 16""")
-    return send_file("./templates/lpips_A_cinematic_view_of_a_castle_in_the_sunset.jpg", as_attachment="lpips_A_cinematic_view_of_a_castle_in_the_sunset.jpg", mimetype="image/jpg")
+    return send_file("/templates/result.jpg", as_attachment="result.jpg", mimetype="image/jpg")
 
 if __name__ == "__main__":
     app.run()
