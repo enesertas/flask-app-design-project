f5ddc863bc355c033003df51d8e7d7df524bdeea
enesertas <enesertas@gmail.com>
2024-04-08 21:34:01 +0300

    v1, code refactoring needed

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, send_file
+from flask_cors import CORS, cross_origin
+from PIL import Image
+from io import BytesIO
+import base64
+import cv2
+import time
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/generate-image", methods=['GET', 'POST'])
+def generate():
+    
+    # generate syncdiffusion image, encode in base64 and send in json
+    time.sleep(3)
+    return send_file("./templates/lpips_A_cinematic_view_of_a_castle_in_the_sunset.jpg", as_attachment="lpips_A_cinematic_view_of_a_castle_in_the_sunset.jpg", mimetype="image/jpg")
+
+if __name__ == "__main__":
+    app.run()
